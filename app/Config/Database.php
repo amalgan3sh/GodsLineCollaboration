72fc937e6a2a89faa7580733875f71de37e4c46e
Amal Ganesh <amalganesh4u@gmail.com>
2024-09-13 15:24:02 +0530

    db setup

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,8 +27,8 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => 'root',
-        'password'     => '',
+        'username'     => 'survey_aranea',
+        'password'     => 'survey_aranea',
         'database'     => 'survey_aranea',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
